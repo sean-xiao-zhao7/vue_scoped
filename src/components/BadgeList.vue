1dde113fdea9de5c754519f4551f71eaa842b570
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-02-22 13:26:11 -0500

    Use slots

--- src/components/BadgeList.vue
+++ src/components/BadgeList.vue
@@ -1,31 +1,31 @@
 <template>
-  <section>
-    <h2>Available Badges</h2>
-    <ul>
-      <li>
-        <base-badge type="admin" caption="ADMIN"></base-badge>
-      </li>
-      <li>
-        <base-badge type="author" caption="AUTHOR"></base-badge>
-      </li>
-    </ul>
-  </section>
+    <base-card>
+        <h2>Available Badges</h2>
+        <ul>
+            <li>
+                <base-badge type="admin" caption="ADMIN"></base-badge>
+            </li>
+            <li>
+                <base-badge type="author" caption="AUTHOR"></base-badge>
+            </li>
+        </ul>
+    </base-card>
 </template>
 
-<style>
+<style scoped>
 section h2 {
-  margin: 0.5rem 0;
-  color: #3a3a3a;
+    margin: 0.5rem 0;
+    color: #3a3a3a;
 }
 ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-  display: flex;
-  flex-direction: row;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: row;
 }
 
 li {
-  margin-right: 1rem;
+    margin-right: 1rem;
 }
 </style>
