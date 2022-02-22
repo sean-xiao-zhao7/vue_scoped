de1ce251f49bd7e2458d197c1f20a2d64478ec24
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-02-22 13:16:10 -0500

    Initial

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <the-header></the-header>
+        <badge-list></badge-list>
+        <user-info
+            :full-name="activeUser.name"
+            :info-text="activeUser.description"
+            :role="activeUser.role"
+        ></user-info>
+    </div>
+</template>
+
+<script>
+import BadgeListVue from "./components/BadgeList.vue";
+import TheHeader from "./components/TheHeader.vue";
+import UserInfoVue from "./components/UserInfo.vue";
+
+export default {
+    components: { "the-header": TheHeader, "badge-list": BadgeListVue, "user-info": UserInfoVue },
+    data() {
+        return {
+            activeUser: {
+                name: "Maximilian Schwarzmüller",
+                description: "Site owner and admin",
+                role: "admin",
+            },
+        };
+    },
+};
+</script>
+
+<style>
+html {
+    font-family: sans-serif;
+}
+
+body {
+    margin: 0;
+}
+</style>
